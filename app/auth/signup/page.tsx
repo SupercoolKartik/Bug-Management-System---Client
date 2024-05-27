230e1bd659e1daa246e85f668fec7b22d2d29730
Suraj <surajkartik71@gmail.com>
2024-05-27 19:27:49 +0530

    Made a signup page using shadcn with zod

---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,206 @@
+"use client";
+import Link from "next/link";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const signupFormSchema = z
+  .object({
+    firstName: z.string().min(1, {
+      message: "First name is required.",
+    }),
+    lastName: z.string().min(1, {
+      message: "Last name is required.",
+    }),
+    email: z.string().email({
+      message: "Please enter a valid email address.",
+    }),
+    phone: z.string().min(10, {
+      message: "Please enter a valid phone no.",
+    }),
+    password: z.string().min(6, {
+      message: "Password must be at least 6 characters.",
+    }),
+    confirmPassword: z.string().min(6, {
+      message: "Confirm password must be at least 6 characters.",
+    }),
+    // acceptTerms: z.boolean().refine((val) => val === true, {
+    //   message: "You must accept the terms and conditions.",
+    // }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match.",
+    path: ["confirmPassword"], // set the path of the error message to 'confirmPassword'
+  });
+
+// export default signupFormSchema;
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const Signup = () => {
+  // Define your form with useForm
+  const form = useForm({
+    resolver: zodResolver(signupFormSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      password: "",
+      confirmPassword: "",
+      // acceptTerms: false,
+    },
+  });
+
+  // Extract handleSubmit, register, and formState from the form object
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = form;
+
+  // // Define your onSubmit function
+  // const onSubmit = (data) => {
+  //   console.log("Form data:", data);
+  // };
+
+  // Defining the submit handler
+  const onSubmit = async (values: z.infer<typeof signupFormSchema>) => {
+    //on Submit logic
+    console.log(values);
+  };
+  return (
+    <div className="container px-5 py-4 mx-auto flex flex-wrap flex-col items-center justify-center">
+      <h1 className="font-bold">Bug Management System</h1>
+      <h3>Sign In to continue</h3>
+      <div className="bg-gray-100 rounded-lg p-7 flex flex-col mt-10 md:mt-0">
+        <Form {...form}>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-2">
+            <FormField
+              control={form.control}
+              name="firstName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>First Name :</FormLabel>
+                  <FormControl>
+                    <Input placeholder="First Name" {...field} />
+                  </FormControl>
+                  <FormMessage>{errors.firstName?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Last Name :</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Last Name" {...field} />
+                  </FormControl>
+                  <FormMessage>{errors.lastName?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email :</FormLabel>
+                  <FormControl>
+                    <Input placeholder="example@mail.com" {...field} />
+                  </FormControl>
+                  <FormMessage>{errors.email?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Phone :</FormLabel>
+                  <FormControl>
+                    <Input placeholder="0123456789" {...field} />
+                  </FormControl>
+                  <FormMessage>{errors.phone?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password :</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="************"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage>{errors.password?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password :</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="************"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage>{errors.confirmPassword?.message}</FormMessage>
+                </FormItem>
+              )}
+            />
+            {/* <FormField
+              control={control}
+              name="acceptTerms"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Checkbox {...field} />
+                  </FormControl>
+                  <FormLabel>Accept Terms and Conditions</FormLabel>
+                  <FormMessage>{errors.acceptTerms?.message}</FormMessage>
+                </FormItem>
+              )}
+            /> */}
+            <Button type="submit">Sign Up</Button>
+            <FormDescription>
+              Already have an account?{" "}
+              <Link
+                href="login"
+                className="text-underline text-purple-700 hover:text-purple-900"
+              >
+                Login
+              </Link>
+            </FormDescription>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
