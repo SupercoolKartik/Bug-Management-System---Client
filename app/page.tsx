35a06578d83c162a490436c7ba8c1d6b2b746a2a
Suraj <surajkartik71@gmail.com>
2024-05-28 18:21:55 +0530

    Decorated the main page a little

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,44 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    console.log(localStorage.getItem("userId"));
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
-      BUG MANAGEMENT SYSTEM
+      Welcome to BUG MANAGEMENT SYSTEM
       <Link href="auth/login">login</Link>
       <Link href="auth/signup">signup</Link>
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 flex flex-col md:flex-row md:mt-10 mt-4">
         <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
           <h2 className="mb-3 text-2xl font-semibold">
-            Tasks{" "}
+            Projects{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+            View all the projects you are a part of.
           </p>
+          //--------- Link to MyProjects page is to be created
         </div>
 
         <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
           <h2 className="mb-3 text-2xl font-semibold">
-            Bugs{" "}
+            Tickets{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            View all the tickets you are assigned to or have assigned.
           </p>
+          //--------- Link to Tickets page is to be created
         </div>
       </div>
     </main>
